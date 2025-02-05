54b28ffbd04b6298089754300e1c197c4d465135
ziouane-rayane <ziouanerayane9@gmail.com>
2025-02-05 11:47:38 +0100

    ITN-1 feat(creation table users)

---
+++ ig_intranet/lib/ig_intranet/mailer.ex
@@ -0,0 +1,3 @@
+defmodule IgIntranet.Mailer do
+  use Swoosh.Mailer, otp_app: :ig_intranet
+end
---
+++ ig_intranet/lib/ig_intranet_web/mon_test.ex
@@ -0,0 +1,9 @@
+defmodule IgIntranetWeb.MonTest do
+  def hello_world do
+    IO.puts "hello"
+  end
+
+  def hello(name) do
+    IO.puts "Hello, " <> name
+  end
+end
