5d841ad4f8a4ff765017a7151283b7c94307d6ef
ziouane-rayane <ziouanerayane9@gmail.com>
2025-02-05 17:10:41 +0100

    ITN-11 ops(this will work)

--- ig_intranet/lib/ig_intranet_web/mon_test.ex
+++ ig_intranet/lib/ig_intranet_web/mon_test.ex
@@ -1,10 +1,12 @@
 defmodule IgIntranetWeb.MonTest do
   def hello_world do
-    IO.puts("hello")
+    IO.puts("hey there")
     IO.puts("hey")
   end
 
   def hello(name) do
-    IO.puts("Hello, " <> name)
+    IO.puts "Hello, " <> name
   end
+
+
 end
