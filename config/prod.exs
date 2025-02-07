f3a993db4f19ec38638f5e74cf9f4784476c72b7
ziouane-rayane <ziouanerayane9@gmail.com>
2025-02-07 16:11:09 +0100

    KAN-2 jf

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,9 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: IgIntranet.Finch
 # Disable Swoosh Local Memory Storage
 config :swoosh, local: false
 
-# Do not print debug messages in production
+# Do not print debug messages in production JF a tort
 config :logger, level: :info
+
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
